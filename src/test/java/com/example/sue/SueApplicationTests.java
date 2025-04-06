eb948e4486699a935821f5c4e351d258666ada3d
hcmutmtk22 <nguyenminhtoan204@gmail.com>
2025-04-06 22:52:58 +0700

    git init

---
+++ src/test/java/com/example/sue/SueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
